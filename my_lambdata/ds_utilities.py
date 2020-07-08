ed36ed3634cfba959bb328d8ca4cb97672be4dfe
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2020-07-07 23:33:30 -0400

    update

--- my_lambdata/ds_utilities.py
+++ my_lambdata/ds_utilities.py
@@ -2,6 +2,7 @@ def enlarge(n):
     '''This function will multiple the input b 100'''
     return n * 100
 
-y = int(input("Choose a number: "))
-print(y, enlarge(y))
+if __name__ == '__main__':
+    y = int(input("Choose a number: "))
+    print(y, enlarge(y))
 
