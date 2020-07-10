869c95646b14e57912ed3d7c5a77047dd46decfe
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2020-07-09 22:20:47 -0400

    new branch three_way_split

--- my_lambdata/ds_utilities.py
+++ my_lambdata/ds_utilities.py
@@ -1,8 +1,25 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_win
+from pdb import set_trace as breakpoint
+
+
 
 def enlarge(n):
     '''This function will multiple the input b 100'''
     return n * 100
+
+# from Bruno Janota U3S1M2 class
+def train_validation_test_split(X, y, train_size=0.7, val_size=0.1, test_size=0.2, random_state=None, shuffle=True):
+            
+    X_train_val, X_test, y_train_val, y_test = train_test_split(
+        X, y, test_size=test_size, random_state=random_state, shuffle=shuffle)
+    
+    X_train, X_val, y_train, y_val = train_test_split(
+        X_train_val, y_train_val, test_size=val_size/(train_size+val_size), 
+        random_state=random_state, shuffle=shuffle)
+    
+    return X_train, X_val, X_test, y_train, y_val, y_test
 
 
 def train_val_test(X):
@@ -169,5 +186,11 @@ def states_full_to_abbr(fullName):
 #     states_full_to_abbr(a)
 
 
+if __name__ == "__main__" :
+    raw_data = load_wine()
+    df = pd.DataFrame(data=raw_data['data'], columns=raw_data['feature_names'])
+    df['target'] = raw_data['target']
+    print(df.shape)
 
+    breakpoint()
 
