447aa914a074e10670ee92883674bdc58f8cf983
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2020-07-17 00:18:24 -0400

    testing 8 passed

--- my_lambdata/ds_utilities.py
+++ my_lambdata/ds_utilities.py
@@ -189,3 +189,82 @@ def states_full_to_abbr(stateFullName):
     }
     return full_to_abbr.get(stateFullName.title())
 
+
+class MyDataSplitter():
+    ''' 
+    This class implements a 3-way data split and outputs summary metrics. 
+    '''
+
+    def _init_(self, df, features, target):
+        self.df = df
+        self.features = features
+        self.target = target
+        self.X = df[features]
+        self.y = df[target]
+
+    def train_validation_test_split(self,
+                                    train_size=0.7, val_size=0.1,
+                                    test_size=0.2, random_state=None,
+                                    shuffle=True):
+        '''
+        This function is a utility wrapper around the Scikit-Learn train_test_split that splits arrays or 
+        matrices into train, validation, and test subsets.
+        Args:
+            X (Numpy array or DataFrame): This is a dataframe with features.
+            y (Numpy array or DataFrame): This is a pandas Series with target.
+            train_size (float or int): Proportion of the dataset to include in the train split (0 to 1).
+            val_size (float or int): Proportion of the dataset to include in the validation split (0 to 1).
+            test_size (float or int): Proportion of the dataset to include in the test split (0 to 1).
+            random_state (int): Controls the shuffling applied to the data before applying the split for reproducibility.
+            shuffle (bool): Whether or not to shuffle the data before splitting
+        Returns:
+            Train, test, and validation dataframes for features (X) and target (y). 
+        '''
+        X_train_val, X_test, y_train_val, y_test = train_test_split(
+            self.X, self.y, test_size=test_size, random_state=random_state, shuffle=shuffle)
+        X_train, X_val, y_train, y_val = train_test_split(
+            X_train_val, y_train_val, test_size=val_size / (train_size + val_size),
+            random_state=random_state, shuffle=shuffle)
+        return X_train, X_val, X_test, y_train, y_val, y_test
+
+    def date_divider(self, date_col):
+        '''
+            Param df: dataframe object from the Pandas library, entire dataframe where the date_column is located is required
+            Param date_col: String value of the name of the date_column to be looked up in the passed dataframe
+            Return: modified dataframe with the new Year, Month, and Day columns attached to the end.
+        '''
+        converted_df = self.df
+        converted_df["Year"] = pd.DatetimeIndex(converted_df[date_col]).year
+        converted_df["Month"] = pd.DatetimeIndex(converted_df[date_col]).month
+        converted_df["Day"] = pd.DatetimeIndex(converted_df[date_col]).day
+        return converted_df
+
+    def print_split_summary(self, X_train, X_val, X_test):
+        '''
+        This function prints summary statistics for X_train, X_val, and X_test.
+        '''
+        print('######################## TRAINING DATA ########################')
+        print(f'X_train Shape: {X_train.shape}')
+        display(X_train.describe(include='all').transpose())
+        print('')
+        print('######################## VALIDATION DATA ######################')
+        print(f'X_val Shape: {X_val.shape}')
+        display(X_val.describe(include='all').transpose())
+        print('')
+        print('######################## TEST DATA ############################')
+        print(f'X_test Shape: {X_test.shape}')
+        display(X_test.describe(include='all').transpose())
+        print('')
+
+
+if __name__ == '_main_':
+    # Test the enlarge function
+    # y = int(input("Choose a number: "))
+    # print(y, enlarge(y))
+    raw_data = load_wine()
+    df = pd.DataFrame(data=raw_data['data'], columns=raw_data['feature_names'])
+    df['target'] = raw_data['target']
+    # Test the My_Data_Splitter Class
+    splitter = MyDataSplitter(df=df, features=['ash', 'hue'], target='target')
+    X_train, X_val, X_test, y_train, y_val, y_test = splitter.train_validation_test_split()
+    splitter.print_split_summary(X_train, X_val, X_test)
--- test/ds_utilities_test.py
+++ test/ds_utilities_test.py
@@ -1,9 +1,9 @@
 import unittest
 import pandas as pd
-from my_lambdata.ds_utilities import enlarge
+from my_lambdata.ds_utilities import enlarge, MyDataSplitter
 from my_lambdata.ds_utilities import states_abbr_to_full
 from my_lambdata.ds_utilities import states_full_to_abbr
-# from my_lambdata.ds_utilities import test_data_divider
+
 
 class TestDsUtilitiesEnlarge(unittest.TestCase):
 
@@ -15,6 +15,26 @@ class TestDsUtilitiesEnlarge(unittest.TestCase):
 
     def test_states_full_to_abbr(self):
         self.assertEqual(states_full_to_abbr("New York"), "NY")
+
+    def test_date_divider(self):
+        # Create MyDataSplitter object
+        splitter = MyDataSplitter()
+        raw_data = {'name': ['Willard Morris', 'Al Jennings', 'Omar Mullins', 'Spencer McDaniel'],
+                    'age': [20, 19, 22, 21],
+                    'favorite_color': ['blue', 'red', 'yellow', "green"],
+                    'grade': [88, 92, 95, 70],
+                    'birth_date': ['01-1996', '08-05-1997', '04-28-1996', '12-16-1995']}
+        splitter.df = pd.DataFrame(raw_data, index = ['Willard Morris', 'Al Jennings', 'Omar Mullins', 'Spencer McDaniel'])
+        date_col = 'birth_date'
+
+        current_shape = splitter.df.shape[1]
+        expected_shape = current_shape + 3
+
+        converted_df = splitter.date_divider(date_col)
+
+        self.assertEqual(expected_shape, converted_df.shape[1])
+
+
 
 
 if __name__ == '__main__':
